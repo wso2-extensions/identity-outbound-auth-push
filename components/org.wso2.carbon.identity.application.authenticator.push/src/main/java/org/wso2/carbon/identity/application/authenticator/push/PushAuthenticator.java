840cbe8654da5d24a8b0ddb7c58db036e64626c8
kushanb <kushan.2017271@iit.ac.lk>
2021-04-22 13:06:59 +0530

    Move request sender to a seperate class

--- components/org.wso2.carbon.identity.application.authenticator.push/src/main/java/org/wso2/carbon/identity/application/authenticator/push/PushAuthenticator.java
+++ components/org.wso2.carbon.identity.application.authenticator.push/src/main/java/org/wso2/carbon/identity/application/authenticator/push/PushAuthenticator.java
@@ -19,14 +19,12 @@
 
 package org.wso2.carbon.identity.application.authenticator.push;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.wso2.carbon.identity.application.authentication.framework.AbstractApplicationAuthenticator;
 import org.wso2.carbon.identity.application.authentication.framework.FederatedApplicationAuthenticator;
-import org.wso2.carbon.identity.application.authentication.framework.cache.AuthenticationContextCacheKey;
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
 import org.wso2.carbon.identity.application.authentication.framework.exception.AuthenticationFailedException;
 import org.wso2.carbon.identity.application.authentication.framework.inbound.InboundConstants;
@@ -42,18 +40,12 @@ import org.wso2.carbon.identity.application.authenticator.push.device.handler.mo
 import org.wso2.carbon.identity.application.authenticator.push.dto.AuthDataDTO;
 import org.wso2.carbon.identity.application.authenticator.push.dto.impl.AuthDataDTOImpl;
 import org.wso2.carbon.identity.application.authenticator.push.exception.PushAuthenticatorException;
-import org.wso2.carbon.identity.application.authenticator.push.internal.PushAuthenticatorServiceComponent;
-import org.wso2.carbon.identity.application.authenticator.push.notification.handler.impl.FirebasePushNotificationSenderImpl;
+import org.wso2.carbon.identity.application.authenticator.push.notification.handler.RequestSender;
+import org.wso2.carbon.identity.application.authenticator.push.notification.handler.impl.RequestSenderImpl;
 import org.wso2.carbon.identity.application.authenticator.push.util.Config;
 import org.wso2.carbon.identity.application.authenticator.push.validator.PushJWTValidator;
 import org.wso2.carbon.identity.application.common.model.Property;
-import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
-import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.user.api.UserStoreException;
-import org.wso2.carbon.user.api.UserStoreManager;
-import org.wso2.carbon.user.core.UserCoreConstants;
-import org.wso2.carbon.user.api.UserRealm;
-import org.wso2.carbon.user.core.service.RealmService;
 
 import java.io.IOException;
 import java.net.URLEncoder;
@@ -61,13 +53,8 @@ import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import ua_parser.Parser;
-import ua_parser.Client;
 
 /**
  * This is the class that implements the push authenticator feature.
@@ -133,7 +120,8 @@ public class PushAuthenticator extends AbstractApplicationAuthenticator
                     new AuthContextCacheEntry(context));
 
             if (deviceList.size() == 1) {
-                sendRequest(request, response, deviceList.get(0).getDeviceId(), sessionDataKey);
+                RequestSender requestSender = new RequestSenderImpl();
+                requestSender.sendRequest(request, response, deviceList.get(0).getDeviceId(), sessionDataKey);
             } else {
 
                 String string = JSONArray.toJSONString(array);
@@ -254,109 +242,12 @@ public class PushAuthenticator extends AbstractApplicationAuthenticator
     }
 
     /**
-     * Send the authentication request to the mobile app using FCM
-     *
-     * @param request  HTTP Request
-     * @param response HTTP Response
-     * @param deviceId Device ID of the authenticating device
-     * @param key      Session Data Key
-     * @throws IOException
-     */
-    public void sendRequest(HttpServletRequest request, HttpServletResponse response,
-                            String deviceId, String key) throws IOException, PushAuthenticatorException {
-
-        DeviceHandler deviceHandler = new DeviceHandlerImpl();
-        Device device;
-        try {
-            device = deviceHandler.getDevice(deviceId);
-        } catch (PushDeviceHandlerClientException e) {
-            throw new PushAuthenticatorException("Error occurred when trying to get device: " + deviceId + ".", e);
-        } catch (SQLException e) {
-            String errorMessage = String
-                    .format("Error when trying to get device: %s from the database.", deviceId);
-            throw new PushAuthenticatorException(errorMessage, e);
-        } catch (PushDeviceHandlerServerException e) {
-            String errorMessage = String
-                    .format("Error occurred when trying to get device: %s. Device may not be registered.", deviceId);
-            throw new PushAuthenticatorException(errorMessage, e);
-        }
-        AuthenticationContext context = AuthContextCache.getInstance().getValueFromCacheByRequestId
-                (new AuthContextcacheKey(key)).getAuthenticationContext();
-
-        AuthenticatedUser user = context.getSequenceConfig().getStepMap().
-                get(context.getCurrentStep() - 1).getAuthenticatedUser();
-        String username = user.getUserName();
-        Map<String, String> authenticatorProperties = context.getAuthenticatorProperties();
-        String serverKey = authenticatorProperties.get(PushAuthenticatorConstants.SERVER_KEY);
-        String fcmUrl = authenticatorProperties.get(PushAuthenticatorConstants.FCM_URL);
-        String hostname = request.getRemoteAddr();
-
-        String serviceProviderName = context.getServiceProviderName();
-
-        String message = username + " is requesting to log into " + serviceProviderName;
-        String sessionDataKey = request.getParameter(InboundConstants.RequestProcessor.CONTEXT_KEY);
-        UUID challenge = UUID.randomUUID();
-        String randomChallenge = challenge.toString();
-        AuthDataDTO authDataDTO = (AuthDataDTO) context.getProperty(PushAuthenticatorConstants.CONTEXT_AUTH_DATA);
-        authDataDTO.setChallenge(randomChallenge);
-        context.setProperty(PushAuthenticatorConstants.CONTEXT_AUTH_DATA, authDataDTO);
-        AuthContextCache.getInstance().addToCacheByRequestId(new AuthContextcacheKey(key),
-                new AuthContextCacheEntry(context));
-
-        String pushId = device.getPushId();
-
-        Map<String, String> userClaims;
-        try {
-            userClaims = getUserClaimValues(user, context);
-        } catch (AuthenticationFailedException e) {
-            throw new PushAuthenticatorException("Error occurred when retrieving user claims for user: "
-                    + user.toFullQualifiedUsername() + ".", e);
-        }
-
-        String fullName =
-                userClaims.get(PushAuthenticatorConstants.FIRST_NAME_CLAIM) + " " +
-                        userClaims.get(PushAuthenticatorConstants.LAST_NAME_CLAIM);
-        String organization = user.getTenantDomain();
-
-        String userAgentString = request.getHeader("user-agent");
-        Parser uaParser = new Parser();
-        Client uaClient = uaParser.parse(userAgentString);
-
-        String userOS = uaClient.os.family;
-        String userBrowser = uaClient.userAgent.family;
-
-        FirebasePushNotificationSenderImpl pushNotificationSender = FirebasePushNotificationSenderImpl.getInstance();
-        pushNotificationSender.init(serverKey, fcmUrl);
-        try {
-            pushNotificationSender.sendPushNotification(deviceId, pushId, message, randomChallenge, sessionDataKey,
-                    username, fullName, organization, serviceProviderName, hostname, userOS, userBrowser);
-        } catch (AuthenticationFailedException e) {
-            throw new PushAuthenticatorException("Error occurred when trying to send the push notification to device: "
-                    + deviceId + ".", e);
-        }
-
-        try {
-            String waitPage = PushAuthenticatorConstants.WAIT_PAGE
-                    + "?sessionDataKey="
-                    + URLEncoder.encode(sessionDataKey, StandardCharsets.UTF_8.name())
-                    + "&challenge="
-                    + URLEncoder.encode(String.valueOf(challenge), StandardCharsets.UTF_8.name());
-            response.sendRedirect(waitPage);
-        } catch (IOException e) {
-            String errorMessage = String.format("Error occurred when trying to to redirect user: %s to the wait page.",
-                    user.toFullQualifiedUsername());
-            throw new PushAuthenticatorException(errorMessage, e);
-        }
-
-    }
-
-    /**
      * Validate the signature using the JWT received from mobile app
      *
      * @param jwt       JWT generated from mobile app
      * @param challenge Challenge stored in cache to correlate with JWT
      * @return Boolean for validity of the signature
-     * @throws PushAuthenticatorException
+     * @throws PushAuthenticatorException Exception in push authenticator
      */
     private boolean validateSignature(String jwt, String challenge)
             throws PushAuthenticatorException {
@@ -385,60 +276,6 @@ public class PushAuthenticator extends AbstractApplicationAuthenticator
                     + " Failed to parse string to JWT.", e);
         }
         return isValid;
-    }
-
-    /**
-     * Get the user claim values for required fields
-     *
-     * @param authenticatedUser Authenticated user
-     * @param context           Authentication context
-     * @return Retrieved user claims
-     * @throws AuthenticationFailedException
-     */
-    private Map<String, String> getUserClaimValues(
-            AuthenticatedUser authenticatedUser, AuthenticationContext context)
-            throws AuthenticationFailedException {
-
-        Map<String, String> claimValues;
-        try {
-            UserRealm userRealm = getUserRealm(authenticatedUser, context);
-            UserStoreManager userStoreManager = userRealm.getUserStoreManager();
-            claimValues = userStoreManager.getUserClaimValues(IdentityUtil.addDomainToName(
-                    authenticatedUser.getUserName(), authenticatedUser.getUserStoreDomain()), new String[]{
-                            PushAuthenticatorConstants.FIRST_NAME_CLAIM,
-                            PushAuthenticatorConstants.LAST_NAME_CLAIM},
-                    UserCoreConstants.DEFAULT_PROFILE);
-        } catch (UserStoreException e) {
-            throw new AuthenticationFailedException("Failed to read user claims for user : "
-                    + authenticatedUser.toFullQualifiedUsername() + ".", e);
-        }
-        return claimValues;
-    }
-
-    /**
-     * Get the user realm of the logged in user.
-     *
-     * @param authenticatedUser Authenticated user.
-     * @return The userRealm.
-     * @throws AuthenticationFailedException Exception on authentication failure.
-     */
-    private UserRealm getUserRealm(
-            AuthenticatedUser authenticatedUser, AuthenticationContext context)
-            throws AuthenticationFailedException {
-
-        UserRealm userRealm = null;
-        try {
-            if (authenticatedUser != null) {
-                String tenantDomain = authenticatedUser.getTenantDomain();
-                int tenantId = IdentityTenantUtil.getTenantId(tenantDomain);
-                RealmService realmService = PushAuthenticatorServiceComponent.getRealmService();
-                userRealm = realmService.getTenantUserRealm(tenantId);
-            }
-        } catch (UserStoreException e) {
-            throw new AuthenticationFailedException("Error occurred when trying to get the user realm for user: "
-                    + authenticatedUser.toFullQualifiedUsername() + ".", e);
-        }
-        return userRealm;
     }
 
 }
---
+++ components/org.wso2.carbon.identity.application.authenticator.push/src/main/java/org/wso2/carbon/identity/application/authenticator/push/notification/handler/RequestSender.java
@@ -0,0 +1,24 @@
+package org.wso2.carbon.identity.application.authenticator.push.notification.handler;
+
+import org.wso2.carbon.identity.application.authenticator.push.exception.PushAuthenticatorException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Processes auth request to be sent as push notifications
+ */
+public interface RequestSender {
+
+    /**
+     * Send the authentication request to the mobile app using FCM
+     *
+     * @param request  HTTP Request
+     * @param response HTTP Response
+     * @param deviceId Device ID of the authenticating device
+     * @param key      Session Data Key
+     * @throws PushAuthenticatorException
+     */
+    void sendRequest(HttpServletRequest request, HttpServletResponse response,
+                     String deviceId, String key) throws PushAuthenticatorException;
+}
