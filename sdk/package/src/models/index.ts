cb658db30ecccc122369f4f99d9a1f1d180449e7
kushanb <kushan.2017271@iit.ac.lk>
2021-06-18 13:46:22 +0530

    Remove unused classes and interfaces

--- sdk/package/src/models/index.ts
+++ sdk/package/src/models/index.ts
@@ -18,7 +18,6 @@
 
 export * from "./accounts";
 export * from "./authRequest";
-export * from "./authResponse";
 export * from "./discoveryData";
 export * from "./registrationRequest";
 export * from "./dateTime";
